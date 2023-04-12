ead943d01412b77628f93df31e7d7c60d56bcfac
unknown <aarushsharma17.96@gmail.com>
2023-04-12 13:09:55 +0000

    Adding Variabless

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 # Configure the Azure provider
 
 
+
 provider "azurerm" {
   features {}
+
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  client_id         = var.client_id
+  client_secret     = var.client_secret
 }
+
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
